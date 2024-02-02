c90dc2aaa7c31a1da6ca0d538b4df36ef09b970c
unknown <zubair@gmail.com>
2024-02-02 20:53:58 +0100

    add navbar to the layout

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function HelloComponents() {
   return (
     <>
       <main class="container-fluid">
-            <Navigation />
+          
             <Component src={socialComponents.nearFortune} />
            
       </main>
